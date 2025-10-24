cdf2a58285ed0f9b54db0bbb14c109f9afe15be4
TamaleAbdulrahman <tamale.rahman12@gmail.com>
2025-10-24 00:49:23 -0700

    Add files via upload

---
+++ media_value/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+# Create your models here.
+import os
+def path_and_rename(instance, filename):
+    upload_to = 'Images/'
+    ext = filename.split('.' )[-1]
+    # get filename
+    if instance.user.username:
+        filename = 'User_Profile_Picture/{}.{}'.format(instance.user.username, ext)
+        return os.path.join(upload_to, filename)
+
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    bio = models.CharField(max_length=200, blank=True)
+
+    profile_pic = models.ImageField(upload_to=path_and_rename, verbose_name="profile", blank=True)
+
+    teacher ='teacher'
+    student ='student'
+    user_types = [
+        (teacher, 'teacher'),
+        (student,'student'),
+    ]
+    user_type = models.CharField(max_length=10, choices=user_types, default=student)
+
+
+    def __str__(self):
+        return self.user.username
+    
+
+class Contact(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.CharField(max_length=150)
+    feedback = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('index')
---
+++ media_value/views.py
@@ -0,0 +1,126 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from media_value.forms import UserForm, UserProfileInfoForm
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.views.generic import TemplateView
+from curriculum.models import Standard
+from .models import UserProfileInfo, Contact
+from django.views.generic import CreateView
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+
+def index(request):
+    return render(request,"home.html")
+
+def register(request):
+    registered = False
+
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.save()
+
+            profile = profile_form.save(commit= False)
+            profile.user = user
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+
+    return render(request, 'media_value/registration.html',
+                             {'registered':registered,
+                              'user_form':user_form,
+                              'profile_form':profile_form})
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("THE ACCOUNT IS DEACTIVATED")
+        else:
+            return HttpResponse("Please use correct name and password")
+    
+
+    else:
+        return render(request, 'media_value/login.html')
+    
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse ('index'))
+
+
+
+# Create your views here.
+# def index(request):
+#     return render(request,'app_users/index.html')
+
+def register(request):
+
+    registered = False
+
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            # user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+    return render(request, 'media_value/registration.html',
+                            {'registered':registered,
+                             'user_form':user_form,
+                             'profile_form':profile_form})
+
+class HomeView(TemplateView):
+    template_name = 'media_value/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        standards = Standard.objects.all()
+        teachers = UserProfileInfo.objects.filter(user_type='teacher')
+        context['standards'] = standards
+        context['teachers'] = teachers
+        return context
+
+class ContactView(CreateView):
+    model = Contact
+    fields = '__all__'
+    template_name = 'media_value/contact.html'
+
+
